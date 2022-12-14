d1d8d37dca9b1b97a7f467ccdfc1e9172559b7e6
rizkihnf <rizkihnf2@gmail.com>
2022-12-15 01:52:25 +0700

    toggle-effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,3 @@ const nav = document.querySelector('.navbar .navbar-nav');
 menuToggle.addEventListener('click', function () {
     nav.classList.toggle('slide');
 });
-
-function changeIcon(icon) {
-    icon.classList.toggle('fa-times');
-}
-
-// changeIcon = (icon) => icon.classList.toggle('fa fa-times');
